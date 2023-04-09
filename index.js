2c4ad8bd5892f32cd1c628431ed7447ad33a09fb
Carlos Zinato <carlosh.lz@hotmail.com>
2023-04-09 17:02:40 +0200

    initial commit with placeholder images

---
+++ index.js
@@ -0,0 +1,33 @@
+const track = document.getElementById('image-track');
+const images = track.getElementsByClassName("image");
+let nextPercentage = 0;
+
+window.onmousedown = e => {
+  track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmousemove = e => {
+  if (track.dataset.mouseDownAt == '0') return;
+
+  const delta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const maxDelta = window.innerWidth / 1;
+  const percentage = (delta / maxDelta) * 100;
+  nextPercentage = parseFloat(track.dataset.mouseUpPercent) + percentage;
+  if (nextPercentage >= 110) nextPercentage = 110;
+  if (nextPercentage <= -10) nextPercentage = -10;
+  track.animate({
+    transform: `translate(${-nextPercentage}%, -50%)`
+  },{
+    duration: 1000,
+    fill: "forwards"
+  })
+  for (const image of images) {
+    image.style.objectPosition = `${100 + nextPercentage}% center`;
+  }
+  
+}
+
+window.onmouseup = e => {
+  track.dataset.mouseDownAt = '0';
+  track.dataset.mouseUpPercent = nextPercentage;
+}
